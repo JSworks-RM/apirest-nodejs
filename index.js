e6c50d3e177784b1cbb28b62bc44fc3ed9836e31
Nacho RM <joseignaciorm13@gmail.com>
2020-01-29 13:55:52 +0100

    Obteniendo el método http, las queries de la url y los headers

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ const url = require('url');
 const servidor = http.createServer((req, res) => { 
     // Obtener la url desde el request. Obtenemos y la parseamos con el método parse()
     let urlRquest = req.url
-        urlParseada = url.parse( urlRquest, true ) // Crea un objeto url -> Url { ... }
+        urlParseada = url.parse( urlRquest, true ) // Con true crea un objeto url -> Url { ... }. Por default es false
         console.log('URL parseada: ', urlParseada, '\n')
         
     // Obtenemos la ruta
     const ruta = urlParseada.pathname
-        console.log(`Ruta: ${ruta} \n`)
+    console.log(`Ruta: ${ruta} \n`)
 
     // Limpiando la ruta quitando los slashes usando método replace()
     const rutaLimpia = ruta.replace(/^\/+|\/+$/g, '')
-        console.log(`Ruta limpia: ${rutaLimpia} \n`)
+    console.log(`Ruta limpia: ${rutaLimpia} \n`)
+    
+    // Obtener método http. Tambien se encuentra dentro del objeto request
+    const metodo = req.method.toLowerCase()
+    console.log(`Método: ${metodo} \n`)
+
+    // Obtenemos las queries de la url
+    const query = urlParseada.query
+    console.log('Query (?key=value): ', JSON.stringify(query))
+
+    // Obtenemos headers: Con headers vemos el contenido del request que vamos a necesitar para el alcance de este proyecto. Con el header podemos ver el tipo de contenido que nos estan enviando, autenticación, para indicar que tipo de aplicación pueden usarlo, entre otros...
+    const headers = req.headers
+    console.log(headers)
     
     // Enviamos la respuesta. 
     // Describiendo una especie de enrutador usando switch para ver los diferentes casos
